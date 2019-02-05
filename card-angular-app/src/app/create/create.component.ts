7554bdb70df9e92e73acab582cddeab1e3a343dc
awildtechno <ajdweavers@gmail.com>
2019-02-05 01:57:39 -0800

    Refactored HTML image elements from URLs into actual HTMLImageElement instances on create form.

--- card-angular-app/src/app/create/create.component.ts
+++ card-angular-app/src/app/create/create.component.ts
@@ -6,34 +6,29 @@ import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms'
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { CdkDragMove, CdkDragEnd } from '@angular/cdk/drag-drop';
 
+const MAX_WIDTH = 1000;
+const MAX_HEIGHT = 1000;
 
 class CardTemplateCustomInput {
     size: number = 3;
     font: string;
-    classes: [string];
     top: number = 0;
-    topdelta: number = 0;
-    leftdelta: number = 0;
     left: number = 0;
 
     constructor(
         public type: string,
-        public options?: object) {
-            console.log(this.type);
-         }
+        public options?: object) { }
 }
 
 class CardTemplate {
     custom_inputs: CardTemplateCustomInput[];
-    height: number;
-    width: number;
-    user_name: string;
-    card_name: string;
+    creator: string;
+    template_name: string;
     category: string;
-    bg_image: string;
+    bg_image: HTMLImageElement;
     constructor() {
         this.custom_inputs = [];
-     }
+    }
 }
 
 @Component({
@@ -47,9 +42,13 @@ class CardTemplate {
 export class CreateComponent implements OnInit {
     template: CardTemplate;
     selectedTab: string = "Banner Text Field";
-    latestUploadedImgUrl: string = '/assets/img/example-image.svg';
-    uploadedImageUrls: string[] = [''];
+    latestUploadedImg: HTMLImageElement;
+    backgroundOptions: HTMLImageElement[];
     form: any;
+
+    // For drag events
+    topdelta: number = 0;
+    leftdelta: number = 0;
 
     constructor(
         private _httpService: HttpService,
@@ -72,11 +71,30 @@ export class CreateComponent implements OnInit {
                     textareadesc: '',
                 }
             }
+            this.backgroundOptions = [];
+            // this.uploadedImages = [];
+            this.latestUploadedImg = new Image();
+            this.latestUploadedImg.src = '/assets/img/example-image.svg';
         }
 
     tabChange(event) {
         console.log(event);
         this.selectedTab = event.tab.textLabel;
+    }
+
+    setTemplateBackground(img) {
+        console.log(img, img.height, img.width);
+        if (img.height > MAX_HEIGHT) {
+            let ratio = (1000 / img.height);
+            img.height = img.height * ratio;
+            img.width = img.width * ratio;
+        }
+        if (img.width > MAX_WIDTH) {
+            let ratio = (1000 / img.width);
+            img.height = img.height * ratio;
+            img.width = img.width * ratio;
+        }
+        this.template.bg_image = img;
     }
 
     addField() {
@@ -88,35 +106,53 @@ export class CreateComponent implements OnInit {
         } else if (this.selectedTab === "Signature Field") {
             this.template.custom_inputs.push(new CardTemplateCustomInput('signature'));
         } else if (this.selectedTab === "Photo") {
+            var img = this.latestUploadedImg;
+            if (img.height > MAX_HEIGHT / 2.5) {
+                let ratio = ((MAX_HEIGHT / 2.5) / img.height);
+                img.height = img.height * ratio;
+                img.width = img.width * ratio;
+                }
+            if (img.width > MAX_WIDTH / 2.5) {
+                let ratio = ((MAX_WIDTH / 2.5) / img.width);
+                img.height = img.height * ratio;
+                img.width = img.width * ratio;
+            }
             this.template.custom_inputs.push(
-                new CardTemplateCustomInput('image', {url: this.latestUploadedImgUrl})
+                new CardTemplateCustomInput('image', {img: img})
                 );
         }
     }
 
-    uploadFileOnChange(event) {
+    uploadFileOnChange(event, target) {
         var file = event.target.files[0];
+        console.log(file);
         var uploadData = new FormData();
         uploadData.append('file', file, file.name);
 
         this._httpService.uploadTemplateFile(uploadData).subscribe((response) => {
-            this.uploadedImageUrls.push(response['filename']);
-            this.latestUploadedImgUrl = response['filename'];
+            var img = new Image();
+            img.src = response['filename'];
+            
+            if (target == 'template') {
+                this.latestUploadedImg = img;
+            } else if (target == 'bg') {
+                this.backgroundOptions.push(img);
+            }
         });
     }
 
     onMove(event: CdkDragMove, index: number) {
         // Please leave this here
-        this.template.custom_inputs[index].leftdelta += event.event['movementX'];
-        this.template.custom_inputs[index].topdelta += event.event['movementY'];
-        console.log(this.template.custom_inputs[index].topdelta);
-        console.log(this.template.custom_inputs[index].leftdelta);
+        this.leftdelta += event.event['movementX'];
+        this.topdelta += event.event['movementY'];
     }
 
     onDrop(event: CdkDragEnd, index: number) {
-        this.template.custom_inputs[index].left += this.template.custom_inputs[index].leftdelta;
-        this.template.custom_inputs[index].top += this.template.custom_inputs[index].topdelta;
+        // Move the objects
+        this.template.custom_inputs[index].left += this.leftdelta;
+        this.template.custom_inputs[index].top += this.topdelta;
 
+        // Correct for dropping out of bounds.
         if (this.template.custom_inputs[index].left >= 1132) {
             this.template.custom_inputs[index].left = 1132;
         } else if (this.template.custom_inputs[index].left < 0) {
@@ -129,9 +165,9 @@ export class CreateComponent implements OnInit {
             this.template.custom_inputs[index].top = 900;
         }
 
-        this.template.custom_inputs[index].leftdelta = 0;
-        this.template.custom_inputs[index].topdelta = 0;
-        console.log(this.template.custom_inputs[index]);
+        // Reset trackers of the change in pixels.
+        this.leftdelta = 0;
+        this.topdelta = 0;
     }
 
     saveTemplate() {
